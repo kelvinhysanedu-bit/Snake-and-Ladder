708cb8ca6677ecc83459ffc7d55d2255049c6b60
kelvinhysanedu-bit <kelvin.hysanedu@gmail.com>
2026-06-15 21:06:54 +0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,117 @@
+const QUESTIONS = {
 
+speaking: [
+
+"What is your favourite food and why?",
+"Describe your hometown.",
+"What do you usually do on weekends?",
+"Tell us about your best friend.",
+"What is your dream job?",
+"Describe your ideal holiday.",
+"What is your favourite movie?",
+"What makes a good teacher?",
+"What is your favourite season?",
+"What is something you are good at?",
+
+"Would you rather live in a city or countryside?",
+"What is your favourite app?",
+"What do you enjoy studying?",
+"Describe a memorable day.",
+"What is your favourite restaurant?",
+"What do you do after school or work?",
+"What is something you want to learn?",
+"What is your favourite sport?",
+"Describe your dream house.",
+"What makes you happy?",
+
+"What is your favourite animal?",
+"What do you usually eat for breakfast?",
+"What is your favourite book?",
+"What would you do with one million dollars?",
+"What country would you like to visit?",
+"What is your favourite subject?",
+"What is your favourite holiday?",
+"What is the most useful invention?",
+"What do you usually do before bed?",
+"What is a good habit everyone should have?"
+
+],
+
+vocabulary: [
+
+"Use 'achievement' in a sentence.",
+"Use 'environment' in a sentence.",
+"Define 'confident'.",
+"Define 'responsible'.",
+"Give a synonym for 'important'.",
+"Give a synonym for 'happy'.",
+"Give an antonym for 'expensive'.",
+"Use 'challenge' in a sentence.",
+"Define 'creative'.",
+"Use 'opportunity' in a sentence.",
+
+"Give a synonym for 'beautiful'.",
+"Define 'successful'.",
+"Use 'improve' in a sentence.",
+"Give an antonym for 'easy'.",
+"Define 'friendly'.",
+"Use 'experience' in a sentence.",
+"Give a synonym for 'large'.",
+"Define 'healthy'.",
+"Use 'adventure' in a sentence.",
+"Give an antonym for 'strong'.",
+
+"Use 'discover' in a sentence.",
+"Define 'ambitious'.",
+"Use 'efficient' in a sentence.",
+"Give a synonym for 'smart'.",
+"Define 'curious'.",
+"Use 'encourage' in a sentence.",
+"Give an antonym for 'early'.",
+"Define 'generous'.",
+"Use 'valuable' in a sentence.",
+"Give a synonym for 'quick'."
+
+],
+
+grammar: [
+
+"Correct: She go to school every day.",
+"Correct: I am play football now.",
+"Correct: They was late yesterday.",
+"Correct: He don't like pizza.",
+"Correct: We has two dogs.",
+
+"Make a question using 'where'.",
+"Make a question using 'when'.",
+"Make a question using 'why'.",
+"Make a question using 'how often'.",
+"Make a question using 'who'.",
+
+"Correct: My brother have a car.",
+"Correct: She can sings well.",
+"Correct: I didn't went there.",
+"Correct: There is many people.",
+"Correct: He studying now.",
+
+"Make a sentence using Present Continuous.",
+"Make a sentence using Present Simple.",
+"Make a sentence using 'because'.",
+"Make a sentence using 'although'.",
+"Make a sentence using 'if'.",
+
+"Correct: She were tired.",
+"Correct: I has finished my homework.",
+"Correct: We doesn't know.",
+"Correct: They is waiting outside.",
+"Correct: He have been here before.",
+
+"Make a question in the past tense.",
+"Make a sentence with 'will'.",
+"Make a sentence with 'should'.",
+"Make a sentence with a comparative adjective.",
+"Make a sentence with a superlative adjective."
+
+]
+
+};
